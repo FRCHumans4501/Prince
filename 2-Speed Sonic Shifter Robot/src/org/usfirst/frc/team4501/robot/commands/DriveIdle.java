041109517d859f7b3a067f7985535ca8175e41a7
Anton Zelkin <antonzelkin@gmail.com>
2016-01-06 14:23:01 -0800

    Uploaded Code To Git

---
+++ 2-Speed Sonic Shifter Robot/src/org/usfirst/frc/team4501/robot/commands/DriveIdle.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team4501.robot.commands;
+
+import org.usfirst.frc.team4501.robot.Robot;
+import org.usfirst.frc.team4501.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveIdle extends Command {
+	
+	DriveTrain driveTrain;
+
+    public DriveIdle() {
+    	requires(Robot.driveTrain);
+    	
+    	driveTrain = Robot.driveTrain;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	driveTrain.arcadeDrive(0, 0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ 2-Speed Sonic Shifter Robot/src/org/usfirst/frc/team4501/robot/subsystems/DriveTrain.java
@@ -0,0 +1,74 @@
+package org.usfirst.frc.team4501.robot.subsystems;
+
+import org.usfirst.frc.team4501.robot.RobotMap;
+import org.usfirst.frc.team4501.robot.commands.DriveIdle;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+	public enum ShifterState{
+		SF_HIGH, SF_LOW;
+	}
+	
+	RobotDrive drive;
+	
+	Talon rightTalon;
+	Talon leftTalon;
+	
+	DoubleSolenoid shifter;
+	ShifterState state;
+	
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public DriveTrain(){
+		this.leftTalon = new Talon(RobotMap.LEFTMOTOR);
+		this.rightTalon = new Talon(RobotMap.RIGHTMOTOR);
+		
+		this.drive = new RobotDrive(leftTalon, rightTalon);
+		this.shifter = new DoubleSolenoid(RobotMap.SOLENOID_HIGHGEAR, RobotMap.SOLENOID_LOWGEAR);
+    }
+    
+    public void initDefaultCommand(){
+    	setDefaultCommand(new DriveIdle());
+    }
+    
+    public void arcadeDrive(double forward, double rotate){
+    	drive.arcadeDrive(forward, rotate);
+    }
+    
+    public void highGear() {
+    	shifter.set(Value.kReverse);
+    	state = ShifterState.SF_HIGH;
+    }
+    
+    public void lowGear() {
+    	shifter.set(Value.kForward);
+    	state = ShifterState.SF_LOW;
+    }
+    
+    public void set(ShifterState state) {
+    	switch (state) {
+    	case SF_HIGH:
+    		this.highGear();
+    		break;
+    		
+    	case SF_LOW:
+    		this.lowGear();
+    		break;
+    	}
+    }
+    public ShifterState getState() {
+    	return state;
+    }
+}
+
